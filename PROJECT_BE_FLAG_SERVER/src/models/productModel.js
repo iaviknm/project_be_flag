52b0e929c0dca152ada4029f4a62ead58dab7a52
iaviknm <nmiavik@gmail.com>
2024-06-12 16:57:11 +0100

    fix error

--- PROJECT_BE_FLAG_SERVER/src/models/productModel.js
+++ PROJECT_BE_FLAG_SERVER/src/models/productModel.js
@@ -2,12 +2,12 @@ const db = require("../db/connection");
 
 async function createProduct(data) {
   try {
-    const { image, name, description, price, inStock, perMeter } = data;
+    const { image, name, description, price, inStock } = data;
     const [result] = await db
       .promise()
       .query(
-        "INSERT INTO products (image, name, description, price, inStock, perMeter) VALUES (?, ?, ?, ?, ?, ?)",
-        [image, name, description, price, inStock, perMeter]
+        "INSERT INTO products (image, name, description, price, inStock) VALUES (?, ?, ?, ?, ?)",
+        [image, name, description, price, inStock]
       );
     return result.insertId;
   } catch (error) {
@@ -16,30 +16,32 @@ async function createProduct(data) {
 }
 
 async function findAllProducts() {
-  const rows = await db.promise().query("SELECT * FROM products");
+  const [rows, fields] = await db.promise().query("SELECT * FROM products");
   return rows;
 }
 
 async function findProductById(id) {
-  const [rows] = await db.promise().query("SELECT * FROM products WHERE id = ?", [
-    id,
-  ]);
+  const [rows] = await db
+    .promise()
+    .query("SELECT * FROM products WHERE id = ?", [id]);
   return rows[0];
 }
 
 async function updateProduct(id, data) {
-  const { image, name, description, price, inStock, perMeter} = data;
-  const [result] = await db.promise().query(
-    "UPDATE products SET image = ?, name = ?, description = ?, price = ?, inStock = ?, perMeter = ? WHERE id = ?",
-    [image, name, description, price, inStock, perMeter, id]
-  );
+  const { image, name, description, price, inStock } = data;
+  const [result] = await db
+    .promise()
+    .query(
+      "UPDATE products SET image = ?, name = ?, description = ?, price = ?, inStock = ?, WHERE id = ?",
+      [image, name, description, price, inStock, id]
+    );
   return result.affectedRows;
 }
 
 async function deleteProduct(id) {
-  const [result] = await db.promise().query("DELETE FROM products WHERE id = ?", [
-    id,
-  ]);
+  const [result] = await db
+    .promise()
+    .query("DELETE FROM products WHERE id = ?", [id]);
   return result.affectedRows;
 }
 
